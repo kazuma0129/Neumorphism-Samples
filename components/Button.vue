a0fc655090e451a10771567a4c150003ebc3ef01
KazumaOhashi <b1016095@fun.ac.jp>
2020-05-09 17:39:43 +0900

    feat: box-sizing

--- components/Button.vue
+++ components/Button.vue
@@ -1,22 +1,34 @@
 <template>
   <v-container
-    class="fabric"
     :style="{
       'background-color': backgroundColor
     }"
   >
-    <v-row align="center" class="ma-10 pa-10" :style="notInsetBoxShadowObject">
+    <v-row
+      align="center"
+      class="rounded-1 ma-xs-1 ma-sm-5 ma-md-10 ma-lg-10 ma-xl-10 pa-xs-3 pa-sm-5 pa-md-10 pa-lg-10 pa-xl-10"
+      :style="notInsetBoxShadowObject"
+    >
       <v-col
         v-for="(i, key) in 6"
         :key="key"
-        cols="2"
+        xs="4"
+        sm="4"
+        md="4"
+        lg="2"
+        xl="2"
         align="center"
         justify="center"
       >
         <v-card
           flat
           shaped
-          :class="[`rounded-${key}`, { 'box-shadow-inset': inset }]"
+          :class="[
+            `rounded-${key}`,
+            { 'box-shadow-inset': inset },
+            `pa-0`,
+            `ma-0`
+          ]"
           width="90"
           height="90"
           min-width="90"
