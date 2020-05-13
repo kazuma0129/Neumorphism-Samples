4b6e9ebeab3995b0d8b32b82074e63929aea01d7
KazumaOhashi <b1016095@fun.ac.jp>
2020-05-13 23:11:33 +0900

    :sparkles: componentization button

---
+++ components/Buttons.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container
+    :style="{
+      'background-color': backgroundColor
+    }"
+  >
+    <v-row
+      align="center"
+      class="rounded-1 ma-xs-1 ma-sm-5 ma-md-10 ma-lg-10 ma-xl-10 pa-xs-3 pa-sm-5 pa-md-10 pa-lg-10 pa-xl-10"
+    >
+      <v-col
+        v-for="(i, key) in 6"
+        :key="key"
+        xs="4"
+        sm="4"
+        md="4"
+        lg="2"
+        xl="2"
+        align="center"
+        justify="center"
+      >
+        <Button
+          :background-color="backgroundColor"
+          :button="true"
+          :round="20"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Button from '~/components/Button'
+export default {
+  components: {
+    Button
+  },
+  props: {
+    backgroundColor: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,35 @@
     <v-container>
       <v-row align="center" class="ma-10 pa-10">
         <v-col align="center">
-          <h2 class="grey--text text--darken-3">Neumorphism Samples</h2>
+          <h2
+            class="display-4 font-weight-black white--text text--darken-3 title"
+          >
+            Neumorphism Samples
+          </h2>
         </v-col>
       </v-row>
     </v-container>
 
+    <v-container>
+      <NeuSwitch :background-color="colors[1]" />
+      <NeuSwitch :background-color="colors[2]" />
+    </v-container>
+
     <v-container v-for="(i, key) in colors" :key="key">
-      <Button :background-color="i" />
+      <Buttons :background-color="i" />
+      <!-- <NeuSwitch :background-color="i" /> -->
     </v-container>
   </v-col>
 </template>
 
 <script>
-import Button from '~/components/Button'
+import Buttons from '~/components/Buttons'
+import NeuSwitch from '~/components/NeuSwitch'
 export default {
-  components: { Button },
+  components: {
+    Buttons,
+    NeuSwitch
+  },
   data() {
     return {
       colors: ['#ffffff', `#ade5df`, `#60cfc6`, '#eeaaff', '#55b9f3', '#b5e2de']
@@ -25,3 +39,8 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+.title
+  text-shadow: rgb(237, 237, 237) 9px 9px 18px, rgb(255, 255, 255) -9px -9px 18px
+</style>
